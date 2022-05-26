a76095400a02f525f01c037438e6ea96d0a4e54f
James Newman <jdnewman85@gmail.com>
2022-05-25 18:00:33 -0600

    Remove custom graph and tree stuffs

--- src/ld2il.rs
+++ src/ld2il.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 type Id = u32;
 
 #[derive(Debug, Default, Copy, Clone, Hash, Eq, PartialEq)]
@@ -18,92 +16,34 @@ pub enum NodeKind {
 
 #[derive(Debug, Clone, Hash, Eq, PartialEq)]
 pub struct Node {
-    pub kind: NodeKind,
     pub id: NodeId,
+    pub kind: NodeKind,
     pub label: String,
 }
 
-#[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
-pub struct ConnectionId { id: Id }
-impl From<Id> for ConnectionId {
-    fn from(id: Id) -> Self {
-        Self { id }
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct Connection {
-    id: ConnectionId,
-    pub sources: HashSet<NodeId>,
-    pub sinks: HashSet<NodeId>,
-}
-
-#[derive(Debug, Clone)]
-pub struct Ladder {
+pub struct NodePool {
     pub nodes: Vec<Node>,
-    pub connections: Vec<Connection>,
-
 }
 
-impl Ladder {
-    pub fn new() -> Ladder {
-        Ladder {
-            //Node is a ladder object, connected to other ladder objects
+impl NodePool {
+    pub fn new() -> NodePool {
+        NodePool {
             nodes: Vec::new(),
-            //Connections are between one or more source nodes and one or more sink nodes
-            connections: Vec::new(),
         }
     }
 
     pub fn new_node<S>(&mut self, kind: NodeKind, label: S) -> NodeId
-    where S: Into<String>,
-    {
+    where S: Into<String> {
         let id = self.nodes.len() as Id;
 
         self.nodes.push(
             Node {
+                id: NodeId{ id },
                 kind,
-                id: id.into(),
                 label: label.into(),
             }
         );
 
-        id.into()
-    }
-
-    pub fn new_connection(&mut self, sources: HashSet<NodeId>, sinks: HashSet<NodeId>) -> ConnectionId
-    {
-        let id = self.connections.len() as Id;
-
-        self.connections.push(
-            Connection {
-                id: id.into(),
-                sources: sources.into(),
-                sinks: sinks.into(),
-            }
-        );
-
-        id.into()
-    }
-
-    pub fn print_node(&self, id: NodeId) {
-        let node = self.nodes[id.id as usize].clone();
-        node.print();
-    }
-}
-
-impl Node {
-    pub fn print(&self) {
-        //println!("Node: {:?}", self);
-        match self.kind {
-            NodeKind::Contact => {
-                //println!("Contact:");
-                println!("PUSH {}", self.label);
-            }
-            NodeKind::Coil => {
-                //println!("Coil:");
-                println!("OUT {}", self.label);
-            }
-        }
+        NodeId { id }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,93 +12,28 @@ pub use crate::ld2il::*;
 use petgraph::graph::Graph;
 use petgraph::dot::{Dot, Config};
 
-use std::collections::HashSet;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
 //    println!("Hello, world!");
 
-    let mut ld = ld2il::Ladder::new();
-
-    let x00 = ld.new_node(ld2il::NodeKind::Contact, "X00");
-    let x01 = ld.new_node(ld2il::NodeKind::Contact, "X01");
-    let x02 = ld.new_node(ld2il::NodeKind::Contact, "X02");
-    let x03 = ld.new_node(ld2il::NodeKind::Contact, "X03");
-    let x04 = ld.new_node(ld2il::NodeKind::Contact, "X04");
-    let x05 = ld.new_node(ld2il::NodeKind::Contact, "X05");
-    let x06 = ld.new_node(ld2il::NodeKind::Contact, "X06");
-    let x07 = ld.new_node(ld2il::NodeKind::Contact, "X07");
-    let x08 = ld.new_node(ld2il::NodeKind::Contact, "X08");
-    let x09 = ld.new_node(ld2il::NodeKind::Contact, "X09");
-    let x10 = ld.new_node(ld2il::NodeKind::Contact, "X10");
-    let x11 = ld.new_node(ld2il::NodeKind::Contact, "X11");
-    let x12 = ld.new_node(ld2il::NodeKind::Contact, "X12");
-    let x13 = ld.new_node(ld2il::NodeKind::Contact, "X13");
-    let y00 = ld.new_node(ld2il::NodeKind::Coil,    "Y00");
-    let y01 = ld.new_node(ld2il::NodeKind::Coil,    "Y01");
-
-    //let l1 =
-    ld.new_connection(
-        HashSet::new(),
-        [x00].into(),
-    );
-    //let a =
-    ld.new_connection(
-        [x00].into(),
-        [x01].into(),
-    );
-    //let b =
-    ld.new_connection(
-        [x01].into(),
-        [x02, x03].into(),
-    );
-    //let c =
-    ld.new_connection(
-        [x02, x03].into(),
-        [x04].into(),
-    );
-    //let d =
-    ld.new_connection(
-        [x04].into(),
-        [x05, x07].into(),
-    );
-    //let e =
-    ld.new_connection(
-        [x05].into(),
-        [x06].into(),
-    );
-    //let f =
-    ld.new_connection(
-        [x07].into(),
-        [x08].into(),
-    );
-    //let g =
-    ld.new_connection(
-        [x06, x08].into(),
-        [x09].into(),
-    );
-    //let h =
-    ld.new_connection(
-        [x09].into(),
-        [x13].into(),
-    );
-    //let i =
-    ld.new_connection(
-        [x13, x10, x12].into(),
-        [y00, y01].into(),
-    );
-    //let j =
-    ld.new_connection(
-        [x11].into(),
-        [x12].into(),
-    );
-    //let l2 =
-    ld.new_connection(
-        [y00, y01].into(),
-        HashSet::new(),
-    );
-
-
+    let mut node_pool = ld2il::NodePool::new();
+    let x00 = node_pool.new_node(ld2il::NodeKind::Coil, "X00");
+    let x01 = node_pool.new_node(ld2il::NodeKind::Coil, "X01");
+    let x02 = node_pool.new_node(ld2il::NodeKind::Coil, "X02");
+    let x03 = node_pool.new_node(ld2il::NodeKind::Coil, "X03");
+    let x04 = node_pool.new_node(ld2il::NodeKind::Coil, "X04");
+    let x05 = node_pool.new_node(ld2il::NodeKind::Coil, "X05");
+    let x06 = node_pool.new_node(ld2il::NodeKind::Coil, "X06");
+    let x07 = node_pool.new_node(ld2il::NodeKind::Coil, "X07");
+    let x08 = node_pool.new_node(ld2il::NodeKind::Coil, "X08");
+    let x09 = node_pool.new_node(ld2il::NodeKind::Coil, "X09");
+    let x10 = node_pool.new_node(ld2il::NodeKind::Coil, "X10");
+    let x11 = node_pool.new_node(ld2il::NodeKind::Coil, "X11");
+    let x12 = node_pool.new_node(ld2il::NodeKind::Coil, "X12");
+    let x13 = node_pool.new_node(ld2il::NodeKind::Coil, "X13");
+    let y00 = node_pool.new_node(ld2il::NodeKind::Coil, "Y00");
+    let y01 = node_pool.new_node(ld2il::NodeKind::Coil, "Y01");
 
     let mut ast = Graph::<NodeId, ()>::new();
     let x00_n = ast.add_node(x00);
@@ -117,10 +52,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let x13_n = ast.add_node(x13);
     let y00_n = ast.add_node(y00);
     let y01_n = ast.add_node(y01);
-    /*
-    let a = ast.add_edge(x00_n, x01_n, ());
-    let b = ast.add_edge(x00_n, x01_n, ());
-    */
+
     ast.extend_with_edges(&[
         (x00_n, x01_n),
         (x01_n, x02_n), (x01_n, x03_n),
