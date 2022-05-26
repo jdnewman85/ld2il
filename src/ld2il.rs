d064f647d35f96536934c03386b953903bc396bd
James Newman <jdnewman85@gmail.com>
2022-05-25 19:58:57 -0600

    Test topological traversal of ladder graph, printing underlying weight and edges

--- src/ld2il.rs
+++ src/ld2il.rs
@@ -1,7 +1,7 @@
 type Id = u32;
 
 #[derive(Debug, Default, Copy, Clone, Hash, Eq, PartialEq)]
-pub struct NodeId { id: Id }
+pub struct NodeId { pub id: Id }
 impl From<Id> for NodeId {
     fn from(id: Id) -> Self {
         Self { id }
@@ -21,6 +21,7 @@ pub struct Node {
     pub label: String,
 }
 
+#[derive(Debug, Clone, Hash, Eq, PartialEq)]
 pub struct NodePool {
     pub nodes: Vec<Node>,
 }
@@ -45,5 +46,56 @@ impl NodePool {
         );
 
         NodeId { id }
+    }
+}
+
+#[derive(Debug, Default, Copy, Clone, Hash, Eq, PartialEq)]
+pub struct AstNodeId { id: Id }
+impl From<Id> for AstNodeId {
+    fn from(id: Id) -> Self {
+        Self { id }
+    }
+}
+
+#[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
+pub enum OperationKind {
+    And,
+    Or,
+}
+
+#[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
+pub enum AstNodeKind {
+    Node(NodeId),
+    Operation(OperationKind),
+}
+
+#[derive(Debug, Clone, Hash, Eq, PartialEq)]
+pub struct AstNode {
+    pub id: AstNodeId,
+    pub kind: AstNodeKind,
+}
+
+pub struct AstNodePool {
+    pub nodes: Vec<AstNode>,
+}
+
+impl AstNodePool {
+    pub fn new() -> AstNodePool {
+        AstNodePool {
+            nodes: Vec::new(),
+        }
+    }
+
+    pub fn new_node(&mut self, kind: AstNodeKind) -> AstNodeId {
+        let id = self.nodes.len() as Id;
+
+        self.nodes.push(
+            AstNode {
+                id: AstNodeId{ id },
+                kind,
+            }
+        );
+
+        AstNodeId { id }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ pub use crate::ld2il::*;
 
 use petgraph::graph::Graph;
 use petgraph::dot::{Dot, Config};
+use petgraph::visit::Topo;
 
 use std::error::Error;
 
@@ -18,42 +19,42 @@ fn main() -> Result<(), Box<dyn Error>> {
 //    println!("Hello, world!");
 
     let mut node_pool = ld2il::NodePool::new();
-    let x00 = node_pool.new_node(ld2il::NodeKind::Coil, "X00");
-    let x01 = node_pool.new_node(ld2il::NodeKind::Coil, "X01");
-    let x02 = node_pool.new_node(ld2il::NodeKind::Coil, "X02");
-    let x03 = node_pool.new_node(ld2il::NodeKind::Coil, "X03");
-    let x04 = node_pool.new_node(ld2il::NodeKind::Coil, "X04");
-    let x05 = node_pool.new_node(ld2il::NodeKind::Coil, "X05");
-    let x06 = node_pool.new_node(ld2il::NodeKind::Coil, "X06");
-    let x07 = node_pool.new_node(ld2il::NodeKind::Coil, "X07");
-    let x08 = node_pool.new_node(ld2il::NodeKind::Coil, "X08");
-    let x09 = node_pool.new_node(ld2il::NodeKind::Coil, "X09");
-    let x10 = node_pool.new_node(ld2il::NodeKind::Coil, "X10");
-    let x11 = node_pool.new_node(ld2il::NodeKind::Coil, "X11");
-    let x12 = node_pool.new_node(ld2il::NodeKind::Coil, "X12");
-    let x13 = node_pool.new_node(ld2il::NodeKind::Coil, "X13");
+    let x00 = node_pool.new_node(ld2il::NodeKind::Contact, "X00");
+    let x01 = node_pool.new_node(ld2il::NodeKind::Contact, "X01");
+    let x02 = node_pool.new_node(ld2il::NodeKind::Contact, "X02");
+    let x03 = node_pool.new_node(ld2il::NodeKind::Contact, "X03");
+    let x04 = node_pool.new_node(ld2il::NodeKind::Contact, "X04");
+    let x05 = node_pool.new_node(ld2il::NodeKind::Contact, "X05");
+    let x06 = node_pool.new_node(ld2il::NodeKind::Contact, "X06");
+    let x07 = node_pool.new_node(ld2il::NodeKind::Contact, "X07");
+    let x08 = node_pool.new_node(ld2il::NodeKind::Contact, "X08");
+    let x09 = node_pool.new_node(ld2il::NodeKind::Contact, "X09");
+    let x10 = node_pool.new_node(ld2il::NodeKind::Contact, "X10");
+    let x11 = node_pool.new_node(ld2il::NodeKind::Contact, "X11");
+    let x12 = node_pool.new_node(ld2il::NodeKind::Contact, "X12");
+    let x13 = node_pool.new_node(ld2il::NodeKind::Contact, "X13");
     let y00 = node_pool.new_node(ld2il::NodeKind::Coil, "Y00");
     let y01 = node_pool.new_node(ld2il::NodeKind::Coil, "Y01");
 
-    let mut ast = Graph::<NodeId, ()>::new();
-    let x00_n = ast.add_node(x00);
-    let x01_n = ast.add_node(x01);
-    let x02_n = ast.add_node(x02);
-    let x03_n = ast.add_node(x03);
-    let x04_n = ast.add_node(x04);
-    let x05_n = ast.add_node(x05);
-    let x06_n = ast.add_node(x06);
-    let x07_n = ast.add_node(x07);
-    let x08_n = ast.add_node(x08);
-    let x09_n = ast.add_node(x09);
-    let x10_n = ast.add_node(x10);
-    let x11_n = ast.add_node(x11);
-    let x12_n = ast.add_node(x12);
-    let x13_n = ast.add_node(x13);
-    let y00_n = ast.add_node(y00);
-    let y01_n = ast.add_node(y01);
+    let mut ld = Graph::<NodeId, ()>::new();
+    let x00_n = ld.add_node(x00);
+    let x01_n = ld.add_node(x01);
+    let x02_n = ld.add_node(x02);
+    let x03_n = ld.add_node(x03);
+    let x04_n = ld.add_node(x04);
+    let x05_n = ld.add_node(x05);
+    let x06_n = ld.add_node(x06);
+    let x07_n = ld.add_node(x07);
+    let x08_n = ld.add_node(x08);
+    let x09_n = ld.add_node(x09);
+    let x10_n = ld.add_node(x10);
+    let x11_n = ld.add_node(x11);
+    let x12_n = ld.add_node(x12);
+    let x13_n = ld.add_node(x13);
+    let y00_n = ld.add_node(y00);
+    let y01_n = ld.add_node(y01);
 
-    ast.extend_with_edges(&[
+    ld.extend_with_edges(&[
         (x00_n, x01_n),
         (x01_n, x02_n), (x01_n, x03_n),
         (x02_n, x04_n), (x03_n, x04_n),
@@ -70,7 +71,21 @@ fn main() -> Result<(), Box<dyn Error>> {
         (x12_n, y00_n), (x12_n, y01_n),
     ]);
 
-    println!("{:?}", Dot::with_config(&ast, &[Config::EdgeNoLabel]));
+    println!("{:?}", Dot::with_config(&ld, &[Config::EdgeNoLabel]));
+
+    let mut bfs = Topo::new(&ld);
+    while let Some(nx) = bfs.next(&ld) {
+        let node_id = ld[nx];
+        println!("\n\nNode: {:?}", node_pool.nodes[node_id.id as usize]);
+
+        for edge in ld.edges_directed(nx, petgraph::Incoming) {
+            println!("In Edge: {:?}", edge);
+        }
+        println!("");
+        for edge in ld.edges_directed(nx, petgraph::Outgoing) {
+            println!("Out Edge: {:?}", edge);
+        }
+    }
 
 
     //dbg!(ld);
