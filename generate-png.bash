26c5cacd598fa23b2ec3b24222e9d97288b99526
James Newman <jdnewman85@gmail.com>
2022-05-29 17:34:20 -0600

    Add early but working reduce_to_or function

--- generate-png.bash
+++ generate-png.bash
@@ -5,3 +5,5 @@ set -euo pipefail
 cargo --color=always run \
 	2> /dev/null \
 	| dot -Tpng > 'test.png'
+
+python -m http.server 8080
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ use petgraph::graph::Graph;
 use petgraph::dot::{Dot, Config};
 use petgraph::graph::NodeIndex;
 
+use std::collections::HashSet;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -86,6 +87,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Reduce
     reduce_into_and(&mut ast_ld, x00_n, x01_n, &mut ast_node_pool);
     reduce_into_and(&mut ast_ld, x05_n, x06_n, &mut ast_node_pool);
+    reduce_into_or (&mut ast_ld, x02_n, x03_n, &mut ast_node_pool);
 
     //dbg!(ast_ld);
     //TODO factor out into function
@@ -103,8 +105,8 @@ fn reduce_into_and(graph: &mut Graph<AstNodeId, ()>, lh_node: NodeIndex, rh_node
     //TODO Return and node?
     //TODO Return Result<>
 
-    // Ensure Node1 sinks are only connected to node2 source
-    //   Single sink/source, and edges_connecting also single
+    //TODO Ensure Node1 sinks are only connected to node2 source
+    //       Single sink/source, and edges_connecting also single
 
     // Store sources from lh_node, and sinks from rh_node
     let lh_sources: Vec<NodeIndex> = graph.neighbors_directed(lh_node, petgraph::Incoming).collect();
@@ -133,6 +135,42 @@ fn reduce_into_and(graph: &mut Graph<AstNodeId, ()>, lh_node: NodeIndex, rh_node
     }
     for sink_node in rh_sinks {
         graph.add_edge(and0_n, sink_node, ());
+    }
+}
+
+fn reduce_into_or(graph: &mut Graph<AstNodeId, ()>, lh_node: NodeIndex, rh_node: NodeIndex, node_pool: &mut AstNodePool) {
+    //TODO Return and node?
+    //TODO Return Result<>
+
+    //TODO Ensure node sinks and sources match exactly
+
+    // Store sources from lh_node, and sinks from rh_node
+    let lh_sources: HashSet<NodeIndex> = graph.neighbors_directed(lh_node, petgraph::Incoming).collect();
+    let rh_sinks:   HashSet<NodeIndex> = graph.neighbors_directed(rh_node, petgraph::Outgoing).collect();
+
+    // Remove nodes and store weights
+    let lh_weight = graph.remove_node(lh_node).unwrap();
+    let rh_weight = graph.remove_node(rh_node).unwrap();
+
+    // Create OR node
+    let or0 = node_pool.new_node(AstNodeKind::Operation(OperationKind::Or));
+    let or0_n = graph.add_node(or0);
+    // Create new nodes for lh, an rh
+    let new_lh_node = graph.add_node(lh_weight);
+    let new_rh_node = graph.add_node(rh_weight);
+
+    // Connect lh and rh nodes to new AND node
+    graph.extend_with_edges(&[
+        (new_lh_node, or0_n),
+        (new_rh_node, or0_n),
+    ]);
+
+    //Reconnect edges based on what existed prior
+    for src_node in lh_sources {
+        graph.add_edge(src_node, or0_n, ());
+    }
+    for sink_node in rh_sinks {
+        graph.add_edge(or0_n, sink_node, ());
     }
 }
 
