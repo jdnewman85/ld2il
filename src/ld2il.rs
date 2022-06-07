bd2255a99f2c5a7849f8ee51fa429a9f1a443be3
James Newman <jdnewman85@gmail.com>
2022-06-06 20:10:09 -0600

    Try everything without using pools and ids - instead using cloneable weight objects

--- src/ld2il.rs
+++ src/ld2il.rs
@@ -1,51 +1,16 @@
-type Id = usize;
-
 #[derive(Debug, Default, Copy, Clone, Hash, Eq, PartialEq)]
-pub struct NodeId { pub id: Id }
-
-#[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
 pub enum NodeKind {
+    #[default]
+    None,
     Contact,
     Coil,
 }
 
-#[derive(Debug, Clone, Hash, Eq, PartialEq)]
+#[derive(Debug, Default, Clone, Hash, Eq, PartialEq)]
 pub struct Node {
-    pub id: NodeId,
     pub kind: NodeKind,
-    pub label: String,
+    pub tag: String,
 }
-
-#[derive(Debug, Clone, Hash, Eq, PartialEq)]
-pub struct NodePool {
-    pub nodes: Vec<Node>,
-}
-
-impl NodePool {
-    pub fn new() -> NodePool {
-        NodePool {
-            nodes: Vec::new(),
-        }
-    }
-
-    pub fn new_node<S>(&mut self, kind: NodeKind, label: S) -> NodeId
-    where S: Into<String> {
-        let id = self.nodes.len() as Id;
-
-        self.nodes.push(
-            Node {
-                id: NodeId{ id },
-                kind,
-                label: label.into(),
-            }
-        );
-
-        NodeId { id }
-    }
-}
-
-#[derive(Debug, Default, Copy, Clone, Hash, Eq, PartialEq)]
-pub struct AstNodeId { pub id: Id }
 
 #[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
 pub enum OperationKind {
@@ -53,39 +18,15 @@ pub enum OperationKind {
     Or,
 }
 
-#[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
+#[derive(Debug, Default, Clone, Hash, Eq, PartialEq)]
 pub enum AstNodeKind {
-    Node(NodeId),
+    #[default]
+    None,
+    Node(Node),
     Operation(OperationKind),
 }
 
-#[derive(Debug, Clone, Hash, Eq, PartialEq)]
+#[derive(Debug, Default, Clone, Hash, Eq, PartialEq)]
 pub struct AstNode {
-    pub id: AstNodeId,
     pub kind: AstNodeKind,
-}
-
-pub struct AstNodePool {
-    pub nodes: Vec<AstNode>,
-}
-
-impl AstNodePool {
-    pub fn new() -> AstNodePool {
-        AstNodePool {
-            nodes: Vec::new(),
-        }
-    }
-
-    pub fn new_node(&mut self, kind: AstNodeKind) -> AstNodeId {
-        let id = self.nodes.len() as Id;
-
-        self.nodes.push(
-            AstNode {
-                id: AstNodeId{ id },
-                kind,
-            }
-        );
-
-        AstNodeId { id }
-    }
 }
